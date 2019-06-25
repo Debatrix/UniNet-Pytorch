a645acff2282f7d90e9c2a0e9dd6d53183f8b56b
debatrix <debatrix@sina.com>
2019-06-25 22:19:20 +0800

    fix:imadjust

--- util/img_enhance.py
+++ util/img_enhance.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import bisect
 
 
 def stretchlim(img, tol=(0.0, 0.99)):
@@ -14,17 +15,53 @@ def stretchlim(img, tol=(0.0, 0.99)):
     return th
 
 
-def imadjust(img, th):
-    img = np.clip(img, th[0], th[1])
-    img = cv2.equalizeHist(img)
-    return img
+def imadjust(src, tol=1, vin=[0, 255], vout=(0, 255)):
+    # https://stackoverflow.com/questions/39767612/what-is-the-equivalent-of-matlabs-imadjust-in-python/44529776
+    # src : input one-layer image (numpy array)
+    # tol : tolerance, from 0 to 100.
+    # vin  : src image bounds
+    # vout : dst image bounds
+    # return : output img
+
+    assert len(src.shape) == 2, 'Input image should be 2-dims'
+
+    tol = max(0, min(100, tol))
+
+    if tol > 0:
+        # Compute in and out limits
+        # Histogram
+        hist = np.histogram(src, bins=list(range(256)), range=(0, 255))[0]
+
+        # Cumulative histogram
+        cum = hist.copy()
+        for i in range(1, 256):
+            cum[i] = cum[i - 1] + hist[i]
+
+        # Compute bounds
+        total = src.shape[0] * src.shape[1]
+        low_bound = total * tol / 100
+        upp_bound = total * (100 - tol) / 100
+        vin[0] = bisect.bisect_left(cum, low_bound)
+        vin[1] = bisect.bisect_left(cum, upp_bound)
+
+    # Stretching
+    scale = (vout[1] - vout[0]) / (vin[1] - vin[0])
+    vs = src - vin[0]
+    vs[src < vin[0]] = 0
+    vd = vs * scale + 0.5 + vout[0]
+    vd[vd > vout[1]] = vout[1]
+    dst = vd
+
+    return dst
 
 
 def adjust_iris(img, r_th=(0.05, 0.95)):
     th = stretchlim(img, (0.0, 0.99))
     rfl = img >= th[1]
     eps_shape = int(np.round(0.005 * img.size))
-    rfl = cv2.morphologyEx(rfl, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (eps_shape, eps_shape)))
+    rfl = cv2.morphologyEx(
+        rfl, cv2.MORPH_CLOSE,
+        cv2.getStructuringElement(cv2.MORPH_RECT, (eps_shape, eps_shape)))
     rfl = cv2.dilate(rfl, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
 
     th = stretchlim(img * (1 - rfl), r_th)
